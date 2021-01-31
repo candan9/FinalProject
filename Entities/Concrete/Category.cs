84cb86e2fe03691ca76c4acf1d1db161fc2e234d
emre candan <wastorist@gmail.com>
2021-01-31 15:05:07 +0300

    Add project files.

---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Category:IEntity
+    {
+        //veritabanındaki tablolara denk gelmeli
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
