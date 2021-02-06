5c7fd5ec3e51cd4e91e3edc560fd7dedc1766856
emre candan <wastorist@gmail.com>
2021-02-06 23:55:30 +0300

    last touchs

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,9 +9,15 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            //ProductTest();
+            //Data Transformation Object
+            ProductTest();
+            //CategoryTest();
+        }
+
+        private static void CategoryTest()
+        {
             CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
-            foreach (var category in  categoryManager.GetAll())
+            foreach (var category in categoryManager.GetAll())
             {
                 Console.WriteLine(category.CategoryName);
             }
@@ -20,9 +26,9 @@ namespace ConsoleUI
         private static void ProductTest()
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetByUnitPrice(2, 1000))
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName+"  " + product.CategoryName);
             }
         }
     }
